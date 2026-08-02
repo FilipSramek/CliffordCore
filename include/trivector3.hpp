10e754ba3ab2c12d832beb1a22bf5f7f25361649
FilipSramek <filip.sid.2004@gmail.com>
2026-08-02 15:51:48 +0200

    Feature, add operators overload for a trivector

--- include/trivector3.hpp
+++ include/trivector3.hpp
@@ -6,12 +6,64 @@ namespace CliffordCore
 {
 
 template<typename T>
+/**
+ * @brief A class representing a trivector in 3D space. Also known as a pseudoscalar, it represents the oriented volume element in 3D space.
+ * @property e123 The trivector component.
+ */
 class Trivector3
 {
     static_assert(std::is_arithmetic<T>::value, "Trivector3 can only be instantiated with numeric types.");
 
 public:
     T e123;
+
+    /**
+     * @brief Default constructor initializes the trivector to zero.
+     */
+    constexpr Trivector3() : e123(0) {}
+    
+    /** 
+     * @brief Constructor initializes the trivector with the provided value.
+     * @param value The value to initialize the trivector with.
+     */
+    constexpr Trivector3(const T& value) : e123(value) {}
+
+    /**
+     * @brief Addition operator overload.
+     * @param other The trivector to add.
+     * @return The resulting trivector.
+     */
+    constexpr Trivector3 operator+(const Trivector3& other) const {
+        return Trivector3(e123 + other.e123);
+    }
+
+    /**
+     * @brief Subtraction operator overload.
+     * @param other The trivector to subtract.
+     * @return The resulting trivector.
+     */
+    constexpr Trivector3 operator-(const Trivector3& other) const {
+        return Trivector3(e123 - other.e123);
+    }
+
+    /**
+     * @brief Multiplication operator overload.
+     * @param other The trivector to multiply with.
+     * @return The resulting trivector.
+     */
+    constexpr Trivector3 operator*(const Trivector3& other) const {
+        return Trivector3(e123 * other.e123);
+    }
+
+    /** 
+     * @brief Division operator overload.
+     * @param other The trivector to divide by.
+     * @return The resulting trivector.
+     */
+    constexpr Trivector3 operator/(const Trivector3& other) const {
+        return Trivector3(e123 / other.e123);
+    }
+
 };
 
 } // namespace CliffordCore
