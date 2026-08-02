0a0c49fc6e7af6bc21a8659ae0c62080f0014231
FilipSramek <filip.sid.2004@gmail.com>
2026-08-02 14:14:48 +0200

    Feature, add geometric reverse of vector and bivector

---
+++ include/operations/reverse.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../vector3.hpp"
+#include "../bivector3.hpp"
+
+namespace CliffordCore
+{
+    template<typename T>
+    /**
+     * @brief Computes the geometric reverse of a 3D vector.
+     * @param v The vector for which to compute the reverse.
+     * @return The reverse of the vector v. (reverse(v) = v)
+     */
+    constexpr Vector3<T> reverse(const Vector3<T>& v) {
+        return Vector3<T>(v.x, v.y, v.z);
+    }
+
+    template<typename T>
+    /**
+     * @brief Computes the geometric reverse of a bivector in 3D space.
+     * @param b The bivector for which to compute the reverse.
+     * @return The reverse of the bivector b. (reverse(b) = -b)
+     */
+    constexpr Bivector3<T> reverse(const Bivector3<T>& b) {
+        return Bivector3<T>(-b.xy, -b.xz, -b.yz);
+    }
+} // namespace CliffordCore
