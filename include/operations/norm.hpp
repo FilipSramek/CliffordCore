614614684499684284ecb2fccf7015df55bc051b
FilipSramek <filip.sid.2004@gmail.com>
2026-08-02 14:17:00 +0200

    Feature, add squared norm

--- include/operations/norm.hpp
+++ include/operations/norm.hpp
@@ -26,4 +26,15 @@ template<typename T>
 constexpr Scalar<T> norm(const Bivector3<T>& b) {
     return Scalar<T>(std::sqrt(b.xy * b.xy + b.xz * b.xz + b.yz * b.yz));  
 }
+
+template<typename T>
+/**
+ * @brief Computes the squared euclidian norm (magnitude) of a 3D vector.
+ * @param v The vector for which to compute the squared norm.
+ * @return The squared euclidian norm (magnitude) of the vector v.
+ */
+constexpr Scalar<T> squared_norm(const Vector3<T>& v) {
+    return Scalar<T>(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
 } // namespace CliffordCore
