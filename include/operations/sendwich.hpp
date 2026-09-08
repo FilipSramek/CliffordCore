b5b49a22d159f2ab2c97ddb8dbd00b289a58b407
FilipSramek <filip.sid.2004@gmail.com>
2026-09-08 19:30:31 +0200

    Bug, fix sandwich product calculations in Vector3 and Rotor3 classes for correct transformation

--- include/operations/sendwich.hpp
+++ include/operations/sendwich.hpp
@@ -14,23 +14,26 @@ namespace CliffordCore
      * @return The resulting vector after the sandwich product.
      */
     constexpr Vector3<T> sandwich(const Vector3<T>& v, const Rotor3<T>& r) {
-        const T s  = r.scalar;
+        const T s  = r.scalar.value;
         const T xy = r.bivector.xy;
         const T xz = r.bivector.xz;
         const T yz = r.bivector.yz;
 
+        // Expanding R v ~R. Note which axis each bivector turns: e12 (xy) spans
+        // the xy plane, so it rotates about z -- not about x. Pairing xy with
+        // the x axis is the easy mistake here.
         return Vector3<T>(
-            (s*s + xy*xy - xz*xz - yz*yz) * v.x
-                + 2 * (xy*xz - s*yz) * v.y
-                + 2 * (xy*yz + s*xz) * v.z,
+            (s*s - xy*xy - xz*xz + yz*yz) * v.x
+                + 2 * (s*xy - xz*yz) * v.y
+                + 2 * (s*xz + xy*yz) * v.z,
 
-            2 * (xy*xz + s*yz) * v.x
+            2 * (-s*xy - xz*yz) * v.x
                 + (s*s - xy*xy + xz*xz - yz*yz) * v.y
-                + 2 * (xz*yz - s*xy) * v.z,
+                + 2 * (s*yz - xy*xz) * v.z,
 
-            2 * (xy*yz - s*xz) * v.x
-                + 2 * (xz*yz + s*xy) * v.y
-                + (s*s - xy*xy - xz*xz + yz*yz) * v.z
+            2 * (-s*xz + xy*yz) * v.x
+                + 2 * (-s*yz - xy*xz) * v.y
+                + (s*s + xy*xy - xz*xz - yz*yz) * v.z
         );
     }
 
