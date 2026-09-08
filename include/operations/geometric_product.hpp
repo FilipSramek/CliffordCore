12249ad1bc96b42cc4fc8e4cf2ef8cb0e8bb84d2
FilipSramek <filip.sid.2004@gmail.com>
2026-09-08 17:48:16 +0200

    Feature, implement geometric product for two 3D rotors

--- include/operations/geometric_product.hpp
+++ include/operations/geometric_product.hpp
@@ -4,6 +4,8 @@
 #include "../vector3.hpp"
 #include "../multivector3.hpp"
 #include "../rotor3.hpp"
+#include "../bivector3.hpp"
+#include "../scalar.hpp" 
 #include "dot_product.hpp"
 #include "wedge_product.hpp"
 
@@ -40,4 +42,39 @@ namespace CliffordCore
             Trivector3<T>(0)
         );
     }
+
+    template<typename T>
+    /**
+     * @brief Computes the geometric product of two 3D rotors, resulting in a 3D rotor. Analogous to two conecutive rotations.
+     * @param r The first rotor.
+     * @param s The second rotor.
+     * @return The resulting rotor from the geometric product.
+     */
+    constexpr Rotor3<T> operator*(const Rotor3<T>& r, const Rotor3<T>& s) {
+
+    Scalar<T> scalar =
+        r.scalar * s.scalar
+        - (r.bivector.xy * s.bivector.xy
+         + r.bivector.xz * s.bivector.xz
+         + r.bivector.yz * s.bivector.yz);
+
+    Bivector3<T> bivector(
+        r.scalar * s.bivector.xy
+        + r.bivector.xy * s.scalar
+        - r.bivector.xz * s.bivector.yz
+        + r.bivector.yz * s.bivector.xz,
+
+        r.scalar * s.bivector.xz
+        + r.bivector.xz * s.scalar
+        + r.bivector.xy * s.bivector.yz
+        - r.bivector.yz * s.bivector.xy,
+
+        r.scalar * s.bivector.yz
+        + r.bivector.yz * s.scalar
+        - r.bivector.xy * s.bivector.xz
+        + r.bivector.xz * s.bivector.xy
+    );
+
+    return Rotor3<T>(scalar, bivector);
+}
 } // namespace CliffordCore
