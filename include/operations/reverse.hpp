cfaa0e93818a90490f391b698a8bf1432951e1ff
FilipSramek <filip.sid.2004@gmail.com>
2026-09-08 18:27:26 +0200

    Feature, implement geometric reverse functions for trivectors and multivectors in 3D space

--- include/operations/reverse.hpp
+++ include/operations/reverse.hpp
@@ -2,6 +2,9 @@
 
 #include "../vector3.hpp"
 #include "../bivector3.hpp"
+#include "../trivector3.hpp"
+#include "../multivector3.hpp"
+#include "../rotor3.hpp"
 
 namespace CliffordCore
 {
@@ -27,12 +30,40 @@ namespace CliffordCore
 
     template<typename T>
     /**
+     * @brief Computes the geometric reverse of a trivector in 3D space.
+     * @param t The trivector for which to compute the reverse.
+     * @return The reverse of the trivector t. (reverse(t) = -t)
+     */
+    constexpr Trivector3<T> reverse(const Trivector3<T>& t) {
+        return Trivector3<T>(-t.value);
+    }
+
+    template<typename T>
+    /**
+     * @brief Computes the geometric reverse of a multivector in 3D space.
+     * @param m The multivector for which to compute the reverse.
+     * @return The reverse of the multivector m. (reverse(m) = scalar(m) + vector(m) - bivector(m) - trivector(m))
+     */
+    constexpr Multivector3<T> reverse(const Multivector3<T>& m) {
+        return Multivector3<T>(
+            m.scalar,
+            m.vector,
+            Bivector3<T>(-m.bivector.xy, -m.bivector.xz, -m.bivector.yz),
+            Trivector3<T>(-m.trivector.value)
+        );
+    }
+
+    template<typename T>
+    /**
      * @brief Computes the geometric reverse of a rotor in 3D space.
      * @param r The rotor for which to compute the reverse.
      * @return The reverse of the rotor r. (reverse(r) = scalar(r) - bivector(r))
      */
-
     constexpr Rotor3<T> reverse(const Rotor3<T>& r) {
         return Rotor3<T>(r.scalar, Bivector3<T>(-r.bivector.xy, -r.bivector.xz, -r.bivector.yz));
     }
+
+    
+
+
 } // namespace CliffordCore
